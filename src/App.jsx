9b9cac8113936940b0f62b191d2b3a8084bc516a
Jerryemmanuel01 <jerryemmanuelolisa@gmail.com>
2024-03-22 15:09:41 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import axios from "axios";
+import { useState } from "react";
+
+function App() {
+  const [data, setData] = useState({});
+  const [location, setLocation] = useState("");
+
+  const API_KEY = "2caa29a9fced729da9d9a4c493c6b031";
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${API_KEY}`;
+
+  const searchLocation = (event) => {
+    if (event.key === "Enter") {
+      axios.get(url).then((response) => {
+        setData(response.data);
+        console.log(response.data);
+      });
+      setLocation("");
+
+    }
+  };
+
+  return (
+    <div className="w-full h-full relative bg-slate-500">
+      <div className="text-center p-4 flex justify-center ">
+        <input
+          type="text"
+          className="px-6 py-3 w-[700px] text-lg rounded-3xl border border-gray-200 text-gray-600 placeholder:text-gray-400 focus:outline-none bg-white-600/100 shadow-md"
+          placeholder="Enter Location"
+          value={location}
+          onChange={(e) => setLocation(e.target.value)}
+          onKeyDown={searchLocation}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default App;
