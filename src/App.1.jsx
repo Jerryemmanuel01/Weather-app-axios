a8d020e6e850dcb01b4ec9ebb635641881a041b9
Jerryemmanuel01 <jerryemmanuelolisa@gmail.com>
2024-03-23 12:40:01 +0100

    react-icons added, search icon with it funtionality also added

---
+++ src/App.1.jsx
@@ -0,0 +1,40 @@
+import axios from "axios";
+import { useState } from "react";
+import { CiSearch } from "react-icons/ci";
+import Weather from "./components/Weather";
+
+export function App() {
+  const [data, setData] = useState({});
+  const [location, setLocation] = useState("");
+
+  const API_KEY = "2caa29a9fced729da9d9a4c493c6b031";
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=${API_KEY}`;
+
+  const searchLocation = (event) => {
+    if (event.key === "Enter") {
+      axios.get(url).then((response) => {
+        setData(response.data);
+        console.log(response.data);
+      });
+      setLocation("");
+    }
+  };
+
+  return (
+    <div className="w-full h-full relative">
+      <div className="text-center p-4 flex justify-center ">
+        <input
+          type="text"
+          className="px-6 py-3 w-[700px] text-lg rounded-3xl border border-gray-200 text-gray-600 placeholder:text-gray-400 focus:outline-none bg-white-600/100 shadow-md"
+          placeholder="Enter Location"
+          value={location}
+          onChange={(e) => setLocation(e.target.value)}
+          onKeyDown={searchLocation}
+        />
+        <CiSearch />
+      </div>
+
+      <Weather weatherData={data} />
+    </div>
+  );
+}
--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { useState } from "react";
+import { CiSearch } from "react-icons/ci";
 import Weather from "./components/Weather";
 
 function App() {
@@ -9,30 +10,37 @@ function App() {
   const API_KEY = "2caa29a9fced729da9d9a4c493c6b031";
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=${API_KEY}`;
 
+  const handleSubmit = () => {
+    axios.get(url).then((response) => {
+      setData(response.data);
+      console.log(response.data);
+    });
+    setLocation("");
+  }
+
   const searchLocation = (event) => {
     if (event.key === "Enter") {
-      axios.get(url).then((response) => {
-        setData(response.data);
-        console.log(response.data);
-      });
-      setLocation("");
+      handleSubmit();
     }
   };
 
   return (
-    <div className="w-full h-full relative">
-      <div className="text-center p-4 flex justify-center ">
-        <input
-          type="text"
-          className="px-6 py-3 w-[700px] text-lg rounded-3xl border border-gray-200 text-gray-600 placeholder:text-gray-400 focus:outline-none bg-white-600/100 shadow-md"
-          placeholder="Enter Location"
-          value={location}
-          onChange={(e) => setLocation(e.target.value)}
-          onKeyDown={searchLocation}
-        />
+    <div className="w-full h-full ">
+      <div className="text-center p-4 ">
+        <div className="flex justify-center relative md:w-[700px] md:mx-auto">
+          <input
+            type="text"
+            className="px-6 py-3 w-[700px] text-lg rounded-3xl border border-gray-200 text-gray-600 placeholder:text-gray-400 focus:outline-none bg-white-600/100 shadow-md"
+            placeholder="Enter Location"
+            value={location}
+            onChange={(e) => setLocation(e.target.value)}
+            onKeyDown={searchLocation}
+          />
+          <CiSearch size={30} className="absolute right-2 cursor-pointer top-2" onClick={handleSubmit}/>
+        </div>
       </div>
-      
-      <Weather weatherData = {data} />
+
+      <Weather weatherData={data} />
     </div>
   );
 }
